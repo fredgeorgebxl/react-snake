bb92c7fb50997614f5fd4f5582e34dc952fe4e0b
Fred-George <f.george@visit.brussels>
2018-06-07 17:21:59 +0200

    font and style

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,17 @@ module.exports = {
               }
             }
           ]
-        }
+        },
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [{
+              loader: 'file-loader',
+              options: {
+                  name: '[name].[ext]',
+                  outputPath: 'fonts/'
+              }
+          }]
+      }
       ]
     },
     output: {
